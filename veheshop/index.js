12796f883db5e0df8a18dcbe54a04a070306eff6
Vk <v@yopmail.com>
2024-03-09 08:25:23 +0530

    minor changes

--- veheshop/index.js
+++ veheshop/index.js
@@ -9,49 +9,59 @@ app.listen(prt, () => {
     console.log(`Server started at port: http://localhost:${prt}`);
 });
 
+var essentials = [
+    {
+        id: 131,
+        imagePath: 'images/product-1.jpg',
+        imageAlt: 'Some product name',
+        discount: 30,
+        price: 120,
+        salePrice: 80,
+        name: 'Bell pepper'
+    },
+    {
+        id: 333,
+        imagePath: 'images/product-2.jpg',
+        imageAlt: 'Some product name',
+        discount: 30,
+        price: 120,
+        salePrice: 80,
+        name: 'Bell pepper'
+    },
+    {
+        id: 41,
+        imagePath: 'images/product-3.jpg',
+        imageAlt: 'Some product name',
+        discount: 30,
+        price: 120,
+        salePrice: 80,
+        name: 'Bell pepper'
+    },
+    {
+        id: 14,
+        imagePath: 'images/product-4.jpg',
+        imageAlt: 'Some product name',
+        discount: 30,
+        price: 120,
+        salePrice: 80,
+        name: 'Bell pepper'
+    },
+    {
+        id: 234,
+        imagePath: 'images/product-5.jpg',
+        imageAlt: 'Some product name',
+        discount: 30,
+        price: 120,
+        salePrice: 80,
+        name: 'Bell pepper'
+    }
+];
+
+app.get('/product/:pid', (req, res) => {
+    var product = essentials.find(product => product.id === pid);
+    res.render('product', product);
+});
+
 app.get('/', (req, res) => {
-    res.render('home',{
-        'essentials' : [
-            {
-                imagePath: 'images/product-1.jpg',
-                imageAlt: 'Some product name',
-                discount: 30,
-                price: 120,
-                salePrice: 80,
-                name: 'Bell pepper'
-            },
-            {
-                imagePath: 'images/product-2.jpg',
-                imageAlt: 'Some product name',
-                discount: 30,
-                price: 120,
-                salePrice: 80,
-                name: 'Bell pepper'
-            },
-            {
-                imagePath: 'images/product-3.jpg',
-                imageAlt: 'Some product name',
-                discount: 30,
-                price: 120,
-                salePrice: 80,
-                name: 'Bell pepper'
-            },
-            {
-                imagePath: 'images/product-4.jpg',
-                imageAlt: 'Some product name',
-                discount: 30,
-                price: 120,
-                salePrice: 80,
-                name: 'Bell pepper'
-            },
-            {
-                imagePath: 'images/product-5.jpg',
-                imageAlt: 'Some product name',
-                discount: 30,
-                price: 120,
-                salePrice: 80,
-                name: 'Bell pepper'
-            }
-        ]
-    });
+    res.render('home', essentials);
 });
