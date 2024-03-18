dbf0b8ee20ef89e5650bf12c6197d31a3f815c49
Dhanuka001 <137430860+Dhanuka001@users.noreply.github.com>
2024-03-18 08:26:33 +0530

    login register modals developed

--- veheshop/index.js
+++ veheshop/index.js
@@ -1,6 +1,14 @@
 const exp = require('express');
 const app = exp();
 const prt = 3000;
+const sql3 = require('sqlite3');
+
+
+
+
+
+
+
 
 app.use(exp.static('public'));
 app.set('view engine', 'ejs');
@@ -17,7 +25,8 @@ var essentials = [
         discount: 30,
         price: 120,
         salePrice: 80,
-        name: 'Bell pepper'
+        name: 'Bell pepper',
+        options: ['Small','Medium','Large','Extra Large']
     },
     {
         id: 333,
@@ -26,7 +35,8 @@ var essentials = [
         discount: 30,
         price: 120,
         salePrice: 80,
-        name: 'Bell pepper'
+        name: 'Bell pepper',
+        options: ['Small','Medium','Large']
     },
     {
         id: 41,
@@ -35,7 +45,8 @@ var essentials = [
         discount: 30,
         price: 120,
         salePrice: 80,
-        name: 'Bell pepper'
+        name: 'Bell pepper',
+        options: ['Small','Extra Large']
     },
     {
         id: 14,
@@ -44,7 +55,8 @@ var essentials = [
         discount: 30,
         price: 120,
         salePrice: 80,
-        name: 'Bell pepper'
+        name: 'Bell pepper',
+        options: ['Small']
     },
     {
         id: 234,
@@ -53,15 +65,52 @@ var essentials = [
         discount: 30,
         price: 120,
         salePrice: 80,
-        name: 'Bell pepper'
+        name: 'Bell pepper',
+        options: ['Medium','Extra Large']
     }
 ];
 
+
+const mainDB = new sql3.Database('iit.shop.db');
+mainDB.run('CREATE TABLE IF NOT EXISTS catalog (pid INTEGER PRIMARY KEY AUTOINCREMENT, imagePath TEXT, imageAlt text,discount DECIMAL INT,price DECIMAL(9,2),salePrice DECIMAL(9,2) ,name TEXT, option TEXT)');
+
+
+/*essentials.forEach(item => {
+
+    mainDB.run('INSERT INTO catalog (imagePath,imageAlt,discount,price,salePrice,name,option) VALUES(?,?,?,?,?,?,?)',
+    item.imagePath, item.imageAlt, item.discount, item.price, item.salePrice, item.name, item.options.toString());
+});*/
+
+function getCatalog(pid = false) {
+    if (pid) {
+        mainDB.get('SELECT * FROM catalog WHERE pid = ?',[pid],(err,row) =>{
+            if (err) {
+                console.error(err);
+                res.status(500).send(err);
+            }else{
+                res.json(row);
+            }
+        });
+    }
+    else{
+        res.json (essentials);
+    }
+}
+
+
+
+
 app.get('/product/:pid', (req, res) => {
-    var product = essentials.find(product => product.id === pid);
+    console.log(req.params.pid);
+    const product = getCatalog(req.params.pid);
+    console.log(product);
+    //var product = essentials.find(product => product.id === req.params.pid);
     res.render('product', product);
 });
 
+
+
 app.get('/', (req, res) => {
-    res.render('home', essentials);
+    res.render('home', { essentials: essentials});
 });
+
